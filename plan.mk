c0bac70fe75f9eac9647f9ec3f3f0a973abf7991
Jakav-N <115905857+Jakav-N@users.noreply.github.com>
2023-05-08 11:57:43 -0600

    Create plan.mk

---
+++ plan.mk
@@ -0,0 +1,5 @@
+# Current APHG review plan
+- Add various review tools
+- Add CSS so the website looks nice
+- Include Quizlets, word lists, I'm not sure what else
+- Make this a useful, but small website
